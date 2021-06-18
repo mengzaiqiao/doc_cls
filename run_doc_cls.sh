4cf41c55b377b98de25e6b96587c90c8753c21de
Zaiqiao Meng <zaiqiao.meng@gmail.com>
2021-06-18 14:37:14 +0100

    change shell

--- run_doc_cls.sh
+++ run_doc_cls.sh
@@ -1,12 +1,38 @@
 start=`date +%s`
 runtime=$((end-start))
-data=$(<text_docs_30.json)
-url=http://192.168.0.26:5000/cls_json
+data=$(<text_docs_10.json)
+url=http://localhost:5000/cls_json
 curl -i \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
--X POST --data $data \
+-X POST --data "$data" \
 $url
 end=`date +%s`
 runtime=$((end-start))
-echo "runtime $runtime seconds"
+echo "10 documents runtime $runtime seconds"
+
+start=`date +%s`
+runtime=$((end-start))
+data=$(<text_docs_20.json)
+url=http://localhost:5000/cls_json
+curl -i \
+-H "Accept: application/json" \
+-H "Content-Type:application/json" \
+-X POST --data "$data" \
+$url
+end=`date +%s`
+runtime=$((end-start))
+echo "20 documents runtime $runtime seconds"
+
+start=`date +%s`
+runtime=$((end-start))
+data=$(<text_docs_30.json)
+url=http://localhost:5000/cls_json
+curl -i \
+-H "Accept: application/json" \
+-H "Content-Type:application/json" \
+-X POST --data "$data" \
+$url
+end=`date +%s`
+runtime=$((end-start))
+echo "30 documents runtime $runtime seconds"
